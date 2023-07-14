dc7bdcb981695960e845030144f19ae0db5e205d
Alessandro Resta <alessandro.resta@proton.me>
2023-07-14 17:44:05 +0300

    Implement logic to handle srt creation

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/alesr/mp4srt/whisperclient"
+)
+
+const (
+	defaultModel string = "whisper-1"
+	inputDir     string = "samples" + string(os.PathSeparator)
+	inputExt     string = ".mp4"
+	outputDir    string = "output" + string(os.PathSeparator)
+	outputExt    string = ".srt"
+	tmpDir       string = "tmp" + string(os.PathSeparator)
+	wavExt       string = ".wav"
+)
+
+func main() {
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		log.Fatalln("missing OPENAI_API_KEY")
+	}
+
+	client := whisperclient.New(apiKey, defaultModel)
+
+	filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+		inputFileName := info.Name()
+		wavFileName := strings.ReplaceAll(inputFileName, inputExt, wavExt)
+		outputFileName := strings.ReplaceAll(inputFileName, inputExt, outputExt)
+
+		if err != nil {
+			log.Fatalf("could not to get stats of '%s' file: %s\n", path, err)
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		cmd := exec.Command(
+			"ffmpeg", "-y", "-i", path, "-vn", "-acodec", "pcm_s16le", "-ar", "5000",
+			"-ac", "2", "-b:a", "32k", tmpDir+wavFileName)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			log.Fatalf("could not to convert '%s' file: %s\n", path, err)
+		}
+
+		wavFile, err := os.Open(tmpDir + wavFileName)
+		if err != nil {
+			log.Fatalf("could not to open '%s' file: %s\n", wavFileName, err)
+		}
+		defer wavFile.Close()
+
+		str, err := client.TranscribeAudio(whisperclient.TranscribeAudioInput{
+			Name: wavFileName,
+			Data: wavFile,
+		})
+		if err != nil {
+			log.Fatalf("could not to transcribe audio: %s\n", err)
+		}
+
+		outputFile := outputDir + outputFileName
+
+		outFile, err := os.Create(outputFile)
+		if err != nil {
+			log.Fatalf("could not to create '%s' output file: %s\n", outputFile, err)
+		}
+		defer outFile.Close()
+
+		if _, err := outFile.Write(str); err != nil {
+			log.Fatalf("could not to write to '%s' output file: %s\n", outputFile, err)
+		}
+		return nil
+	})
+}
